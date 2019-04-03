49a2341cdc582c6b938f1d61b9bc9a3faf657ad3
nrm176 <nerimplo.tokyo@gmail.com>
2019-04-03 10:31:27 +0900

    add nav and global css

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,27 @@ export default {
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {charset: 'utf-8'},
+
+      /* ロボット対策 */
+      { name: 'robots', content: 'noindex'},
+      { name: 'robots', content: 'nofollow'},
+
+      /* 互換表示させないIE対策 */
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+
+      /* モバイル対応 */
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
+      /* 著者情報 */
+      { name: 'author', content: 'nerimplo' },
+
+      /* OGPタグ */
+      { hid: 'og:site_name', property: 'og:site_name', content: 'サイト名' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'サイトURL' },
+      { hid: 'og:title', property: 'og:title', content: 'サイト名' },
+      { hid: 'og:description', property: 'og:description', content: '共通ディスクリプション' },
+      { hid: 'og:image', property: 'og:image', content: 'サイトURL/ogp/common.jpg' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
@@ -27,6 +46,8 @@ export default {
   ** Global CSS
   */
   css: [
+    'normalize.css',
+    '~/static/css/global.css'
   ],
 
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <section>
-    <h1>Hello</h1>
-  </section>
+  <div>
+    <Nav/>
+    <section>
+      <h1>Hello</h1>
+    </section>
+  </div>
 </template>
 
 <script>
 
+import Nav from "../components/Nav";
 export default {
   components: {
+    Nav
   }
 }
 </script>
