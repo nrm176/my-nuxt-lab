d2b3748147188ce9a8eafd4647674cfc2a870e98
nrm176 <nerimplo.tokyo@gmail.com>
2019-04-23 12:35:59 +0900

    cloudinary + animated css

---
+++ components/Cloudinary.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "Cloudinary",
+    head() {
+      return {
+        script: [
+          {src: 'https://widget.cloudinary.com/v2.0/global/all.js'}
+        ],
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
