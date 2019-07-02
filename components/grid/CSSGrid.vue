75937c4f4621cd568fad868932b62885bd60ca7c
nrm176 <nerimplo.tokyo@gmail.com>
2019-07-02 23:19:34 +0900

    change css grid

--- components/grid/CSSGrid.vue
+++ components/grid/CSSGrid.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <div class="app-container">
-      <div class="app-box" v-for="stock in stocks">
-        <div class="card">
+      <div class="app-box" v-for="stock in stocksv2">
+        <div class="card" :class="stock.rank">
           <div class="card-container">
-              <div class="pill">
-                <div class="text-align-center">{{stock.cName}}</div>
-              </div>
-
-            <p>{{ stock.pxChange}}</p>
+            <div class="pill">
+              <div class="text-align-center text-style">{{stock.c_name}}</div>
+            </div>
+            <div class="text-align-right">{{stock.px}} ({{ stock.chg}})</div>
           </div>
         </div>
       </div>
@@ -19,19 +18,20 @@
 </template>
 
 <script>
-  const data = require('@/assets/json/stocks.json')
+  const datav2 = require('@/assets/json/stocks.v2.json')
   export default {
     name: "CSSGrid",
     data() {
       return {
-        raw_data: data.items
+        // raw_data: data.items,
+        stocksv2: datav2
       }
     },
-    computed: {
-      stocks() {
-        return [].concat.apply([], this.raw_data.map((e)=>e.children))
-      }
-    }
+    // computed: {
+    //   stocks() {
+    //     return [].concat.apply([], this.raw_data.map((e) => e.children))
+    //   },
+    // }
   }
 </script>
 
@@ -93,4 +93,115 @@
   .text-align-center {
     text-align: center;
   }
+
+  .text-align-right {
+    text-align: right;
+  }
+
+
+  .p-0 {
+    background-color: #ffebee;
+  }
+
+
+  .p-1 {
+    background-color: #ffcdd2;
+  }
+
+
+  .p-2 {
+    background-color: #ef9a9a;
+  }
+
+
+  .p-3 {
+    background-color: #e57373;
+  }
+
+
+  .p-4 {
+    background-color: #ef5350;
+  }
+
+
+  .p-5 {
+    background-color: #f44336;
+  }
+
+
+  .p-6 {
+    background-color: #e53935;
+  }
+
+
+  .p-7 {
+    background-color: #d32f2f;
+  }
+
+
+  .p-8 {
+    background-color: #c62828;
+  }
+
+
+  .p-9 {
+    background-color: #b71c1c;
+  }
+
+
+  .n-0 {
+    background-color: #e3f2fd;
+  }
+
+
+  .n-1 {
+    background-color: #bbdefb;
+  }
+
+
+  .n-2 {
+    background-color: #90caf9;
+  }
+
+
+  .n-3 {
+    background-color: #64b5f6;
+  }
+
+
+  .n-4 {
+    background-color: #42a5f5;
+  }
+
+
+  .n-5 {
+    background-color: #2196f3;
+  }
+
+
+  .n-6 {
+    background-color: #1e88e5;
+  }
+
+
+  .n-7 {
+    background-color: #1976d2;
+  }
+
+
+  .n-8 {
+    background-color: #1565c0;
+  }
+
+
+  .n-9 {
+    background-color: #0d47a1;
+  }
+
+  .text-style {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
 </style>
